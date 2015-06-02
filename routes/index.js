6e6b3e06b4a431cfdecd667f17f0e3a51a1e2e38
Javi de la Haza <javi.delahaza@gmail.com>
2015-06-02 10:55:27 +0200

    Primera pagina y favicon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+//Utilizamos el generador de router para gestionar las rutas concretas
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
