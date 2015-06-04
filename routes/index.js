53112b5d6e66f3d9c81beed6c81650946791d708
Javi de la Haza <javi.delahaza@gmail.com>
2015-06-04 11:07:31 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,18 @@ var express = require('express');
 //Utilizamos el generador de router para gestionar las rutas concretas
 var router = express.Router();
 
+var quizController = require ('../controllers/quiz_controller');
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Se viene por esta ruta, renderizara la vista en la que se vera
+// la pregunta con el cuadro de texto para introducir la respuesta
+router.get('/quizes/question', quizController.question);
+// Si viene por esta ruta, en fuuncion de la respuesta se mostrara si la respuesta ha sido correcta o no
+router.get('/quizes/answer'  , quizController.answer);
 
 module.exports = router;
