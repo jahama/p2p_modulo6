9e33e758a911ff9ef5bf490685a56ecc8b3de8c7
Javi de la Haza <javi.delahaza@gmail.com>
2015-06-07 02:38:01 +0200

    Adaptar la configuración de sequelize.js para que en el entorno local use la base de

--- models/models.js
+++ models/models.js
@@ -1,15 +1,36 @@
 var path= require('path');
+/***** Adaptar el modelo a despliegue en Heroku *****/
+// Postgress DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite    DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user 	 = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 
+console.log(url);
 //Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null,
-						{dialect:"sqlite", storage: "quiz.sqlite"}
+// Usar BBDD SQLite o POSTGRES
+var sequelize = new Sequelize(DB_name, user, pwd,
+						{
+							dialect  : protocol,
+							protocol : protocol,
+							port     : port,
+							host     : host,
+							storage  : storage,  // Solo SQLite(.env)
+							omitNull : true	     // Solo Postgres
+						}
 					);
 console.log(sequelize);
 // Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz')
+var Quiz = sequelize.import(quiz_path);
 // exportar definicion de la tabla Quiz
 exports.Quiz = Quiz;
 
